fb93fdc8a758cf26bb2bbf23e448d975cd6ff7d9
keccah <rporphirio@gmail.com>
2014-07-26 18:21:46 -0300

    Now my function with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Basically applied the same priciples to the example provided
+## in the Programming Assignment 2. 
 
-## Write a short comment describing this function
+## In the makeCacheMatrix, I create a function that gets an argument
+## which will be a matrix. There's an inner function that holds the value 
+## the variable and turns it into a matrix.
+## Soon after that it inverts the matrix and holds it into the getsolve object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y) {
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) mat <<- solve
+  getsolve <- function() mat
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function tests the value of the object already solved 
+## in the function makeCacheMatrix and sees if it's null.
+## If it's not, it prints that. If it is, it solves that.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$getsolve()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+}
 }
